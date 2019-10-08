c481bc57ad6327ded574ecad3f667cc64cbf1331
Sergeichik Vladimir <sergeichik.vs@phystech.edu>
2019-10-08 18:40:20 +0300

    ))

--- lab3/3_2.py
+++ lab3/3_2.py
@@ -4,7 +4,6 @@ windowSize(1000, 600)
 canvasSize(1000, 600)
 
 
-
 def ellipse(a, b, x0, y0):
     x = a
     y = 0
@@ -38,6 +37,7 @@ def man(a, b):
     line(a - 25, b + 200, a - 65, b + 330)
     line(a - 65, b + 330, a - 100, b + 330)
 
+
 def woman(a, b):
     penColor(233, 99, 233)
     brushColor(233, 99, 233)
@@ -55,10 +55,61 @@ def woman(a, b):
     line(a + 12, b + 60, a + 50, b + 100)
     line(a + 50, b + 100, a + 120, b + 65)
     line(a - 12, b + 60, a - 82, b + 140)
-def mirror ()
-man(200,100)
-woman(360,100)
 
 
+def mirorwoman(a, b):
+    penColor(233, 99, 233)
+    brushColor(233, 99, 233)
+    polygon([(a, b + 30), (a - 75, b + 220), (a + 75, b + 220), (a, b + 30)])
+    penColor(229, 194, 152)
+    brushColor(229, 194, 152)
+    circle(a, b, 45)
+    penColor(0, 0, 0)
+    # отрисовывает ноги : первые две строки - правую, а вторые две строки - левую;
+    line(a - 20, b + 220, a - 20, b + 330)
+    line(a - 20, b + 330, a - 40, b + 330)
+    line(a + 20, b + 220, a + 20, b + 330)
+    line(a + 20, b + 330, a + 40, b + 330)
+    # отрисовывает руки : первая строка - правую, а вторая строка - левую;
+    line(a - 12, b + 60, a - 50, b + 100)
+    line(a - 50, b + 100, a - 120, b + 65)
+    line(a + 12, b + 60, a + 82, b + 140)
 
+
+def heart(a,b):
+    line(a, b, a - 20, b - 80)
+    penColor(255, 0, 0)
+    brushColor(255, 0, 0)
+    polygon([(a - 20, b - 80), (a - 7, b - 140), (a - 62, b - 120), (a - 20, b - 80)])
+    circle(a - 25, b - 135, 18)
+    circle(a - 47, b - 128, 18)
+
+
+def ice_cream(a, b):
+    penColor(255, 215, 0)
+    brushColor(255, 215, 0)
+    polygon([(a, b), (a + 20, b - 100), (a + 58, b - 65), (a, b)])
+    penColor(139, 69, 19)
+    brushColor(139, 69, 19)
+    ellipse(14, 10, a + 47, b - 70)
+    penColor(255, 0, 0)
+    brushColor(255, 0, 0)
+    ellipse(14, 12, a + 32, b - 95)
+    penColor(255, 255, 255)
+    brushColor(255, 255, 255)
+    ellipse(15, 12, a + 49, b - 88)
+
+brushColor(80,230,230)
+rectangle(0, 0, 1000, 250)
+brushColor(0,128,85)
+rectangle(0, 250, 1000, 600)
+man(200, 200)
+woman(360, 200)
+mirorwoman(600, 200)
+man(772, 200)
+heart(110, 340)
+ice_cream(852, 340)
+penColor(0, 0, 0)
+line(480, 265, 520, 180)
+ice_cream(520, 180)
 run()
